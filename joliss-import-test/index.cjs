60c20053969b32e674e514268ff9c5556626571c
Jo Liss <joliss42@gmail.com>
2022-11-22 16:23:00 +0000

    Add `??` tests

---
+++ joliss-import-test/index.cjs
@@ -0,0 +1,3 @@
+let x = eval("1")
+let y = eval("1")
+console.log("NULLISH: node_modules/joliss-import-test/index.cjs", x ?? y)
---
+++ joliss-import-test/index.js
@@ -0,0 +1,5 @@
+let x = eval("1")
+let y = eval("1")
+console.log("NULLISH: node_modules/joliss-import-test/index.js", x ?? y)
+
+export {}
